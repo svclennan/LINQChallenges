0723279054d171336f95e1d0ab53fe746f8f989a
unknown <svclennan@icloud.com>
2020-02-07 14:46:23 -0600

    finished problems with an average function using both LINQ and without LINQ

--- LINQPractice/Program.cs
+++ LINQPractice/Program.cs
@@ -10,26 +10,87 @@ namespace LINQPractice
     {
         static void Main(string[] args)
         {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> output = SubstringTH(words);
+            foreach (string word in output)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
 
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Ian", "Mike" };
+            output = Duplicates(names);
+            foreach (string word in output)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine();
+
+            List<string> classGrades = new List<string>() { "80,100,92,89,65", "93,81,78,84,69", "73,88,83,99,64", "98,100,66,74,55" };
+            Console.WriteLine(AverageLINQ(classGrades));
+            Console.WriteLine();
+
+            string name = "Sean CLennan";
+            Console.WriteLine(LetterFrequency(name));
+            Console.WriteLine();
+            Console.ReadLine();
         }
-        public static List<string> thSubstring(List<string> words)
+        public static List<string> SubstringTH(List<string> words)
         {
-
+            List<string> result = new List<string>();
+            var substrings = words.Where(w => w.Contains("th"));
+            foreach (var word in substrings)
+            {
+                result.Add(word);
+            }
+            return result;
         }
-
+        
         public static List<string> Duplicates(List<string> words)
         {
-
+            var result = words.Distinct();
+            return result.ToList();
         }
-
+        
         public static double Averages(List<string> grades)
         {
+            double avg;
+            List<double> studentAverage = new List<double>();
+            foreach (var studentGrade in grades)
+            {
+                string[] strings = studentGrade.Split(',');
+                List<int> integers = new List<int>();
+                foreach (string character in strings)
+                {
+                    integers.Add(Convert.ToInt32(character));
+                }
+                integers.Remove(integers.Min());
+                studentAverage.Add(integers.Average());
+            }
+            avg = studentAverage.Average();
+            return avg;
+        }
 
+        public static double AverageLINQ(List<string> grades)
+        {
+            return grades.Select(g => g.Split(','))
+                         .Select(y => y.Select(z => Double.Parse(z)).OrderBy(x => x).Skip(1))
+                         .SelectMany(x => x).Average();
         }
 
         public static string LetterFrequency(string name)
         {
-
+            string output = "";
+            var result =
+                from n in name.ToUpper().ToArray()
+                group n by n into letterFrequency
+                orderby letterFrequency.Key
+                select letterFrequency;
+            foreach (var value in result)
+            {
+                output += value.Key.ToString() + value.Count();
+            }
+            return output;
         }
     }
 }
