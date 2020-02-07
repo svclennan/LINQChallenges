9f5a20f812f41a6ee9a783ed14fc8ff872fa4ecc
unknown <svclennan@icloud.com>
2020-02-07 10:16:12 -0600

    init

---
+++ LINQPractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        public static List<string> thSubstring(List<string> words)
+        {
+
+        }
+
+        public static List<string> Duplicates(List<string> words)
+        {
+
+        }
+
+        public static double Averages(List<string> grades)
+        {
+
+        }
+
+        public static string LetterFrequency(string name)
+        {
+
+        }
+    }
+}
